32ea8a72d46cdc2a2964f0eeb92f23951f53bd20
Hitallo-moreira <hitallomoreira13@gmail.com>
2023-06-15 18:02:38 -0300

    feat: create header component and style

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import './App.css'
+import Header from './Header'
 import './styles/styles.scss'
 
 function App() {
 
   return (
-    <h1>teste</h1>
+      <Header />
   )
 }
 
---
+++ src/Header.jsx
@@ -0,0 +1,29 @@
+function Header() {
+    return (      
+        <header className="container">
+            <div className="content">
+                <div className="left-content">
+                    <div className="logo">
+                        <p>H</p>
+                    </div>
+                    <nav>
+                        <ul>
+                            <li><a href="#">Sobre</a></li>
+                            <li><a href="#">Projetos</a></li>
+                            <li><a href="#">Contato</a></li>
+                        </ul>
+                    </nav>
+                </div>
+                <div className="right-content">
+                    <div className="action">
+                        <div className="btn-download">
+                            <input type="submit" value="Baixar CV" />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>    
+    )
+}
+
+export default Header
