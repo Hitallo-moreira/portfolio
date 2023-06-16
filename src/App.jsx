b8c5ffd6dffbe0cae2da1bb0b0278404a5f6b09f
Hitallo-moreira <hitallomoreira13@gmail.com>
2023-06-16 15:32:12 -0300

    feat: create hero section component and style

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import Header from './Header'
+import HeroSection from './HeroSection'
 import './styles/styles.scss'
+
 
 function App() {
 
   return (
+    <div>
       <Header />
+      <HeroSection />
+    </div>
   )
 }
 
---
+++ src/HeroSection.jsx
@@ -0,0 +1,9 @@
+function HeroSection() {
+    return (
+        <div className="container hero-section">
+            <h1>Hitallo Moreira</h1>
+        </div>
+    )
+}
+
+export default HeroSection
