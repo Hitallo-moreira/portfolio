0f4135fa6148136eabe373dbd234932949052322
Hitallo-moreira <hitallomoreira13@gmail.com>
2023-06-16 17:30:40 -0300

    feat: change hero-section

--- src/HeroSection.jsx
+++
@@ -1,9 +0,0 @@
-function HeroSection() {
-    return (
-        <div className="container hero-section">
-            <h1>Hitallo Moreira</h1>
-        </div>
-    )
-}
-
-export default HeroSection
