c234a2505a19c1ef4aef951fafe86f075d1735fd
Hitallo-moreira <hitallomoreira13@gmail.com>
2023-06-20 13:05:13 -0300

    feat: create about-section component and style

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from './components/Header'
 import HeroSection from './components/HeroSection'
+import AboutSection from './components/AboutSection'
 import './styles/styles.scss' 
 
 
@@ -9,6 +10,7 @@ function App() {
     <div>
       <Header />
       <HeroSection />
+      <AboutSection />
     </div>
   )
 }
---
+++ src/components/AboutSection.jsx
@@ -0,0 +1,40 @@
+import MyCarousel from "./Carousel"
+
+function AboutSection() {
+    return (
+        <div className="about">
+            <div className="container about-section">
+                <div className="about-align">
+                    <div className="title-line"></div>
+                    <p className="about-title">Sobre</p>
+                </div>
+                <div className="row">
+                    <div className="col">
+                        <h2 className="title">Desenvolvedor Front-end</h2>
+                        <p className="about-me">
+                            Focado em desenvolvimento Web, com 1 ano de atuação no mercado. Continuo explorando novas maneiras de criar experiências digitais impactantes.
+                        </p>
+                    </div>
+                    <div className="col">
+                        <div className="stacks">
+                            <h2 className="title">Stacks</h2>
+                            <p className="my-stacks">
+                                Possuo conhecimento nas seguintes tecnologias:
+                            </p>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col">
+                            <div className="carousel">
+                                <MyCarousel />  
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default AboutSection
+
