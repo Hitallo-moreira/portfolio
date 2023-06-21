a653f6bd848ff73e7df7585d65a70f4063b758bc
Hitallo-moreira <hitallomoreira13@gmail.com>
2023-06-20 21:43:11 -0300

    feat: create footer and style

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from './components/Header'
 import HeroSection from './components/HeroSection'
 import AboutSection from './components/AboutSection'
 import ProjectSection from './components/ProjectSection'
+import Footer from './components/Footer'
 import './styles/styles.scss' 
 
 
@@ -13,6 +14,7 @@ function App() {
       <HeroSection />
       <AboutSection />
       <ProjectSection />
+      <Footer />
     </div>
   )
 }
