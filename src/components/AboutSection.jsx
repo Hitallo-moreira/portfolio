d09a7d1a6677c2dc6159f7e74f9cc27ee75c790e
Hitallo-moreira <hitallomoreira13@gmail.com>
2023-06-26 14:27:56 -0300

    feat: add responsive style

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,6 +1,22 @@
+import React, { useState, useEffect } from 'react';
 import MyCarousel from "./Carousel"
 
 function AboutSection() {
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        function handleWindowResize() {
+            setIsMobile(window.innerWidth <= 768);
+        }
+
+        handleWindowResize();
+        window.addEventListener('resize', handleWindowResize);
+
+        return () => {
+            window.removeEventListener('resize', handleWindowResize);
+        };
+    }, []);
+
     return (
         <div id="about">
             <div className="container about-section">
@@ -8,21 +24,21 @@ function AboutSection() {
                     <div className="title-line"></div>
                     <p className="about-title">Sobre</p>
                 </div>
-                <div className="row">
-                    <div className="col">
+                <div className='my-info'>
+                    <div className="info">
                         <h2 className="title">Desenvolvedor Front-end</h2>
                         <p className="about-me">
-                            Focado em desenvolvimento Web, com 1 ano de atuação no mercado. Continuo explorando novas maneiras de criar experiências digitais impactantes.
+                            {isMobile ? 'Com 1 ano de atuação no mercado, Continuo explorando novas maneiras de criar experiências digitais impactantes.' : 'Focado em desenvolvimento Web, com 1 ano de atuação no mercado, continuo explorando novas maneiras de criar experiências digitais impactantes.'}
                         </p>
-                    </div>
-                    <div className="col">
+                    </div>   
+                    <div className="tech">
                         <div className="stacks">
                             <h2 className="title">Stacks</h2>
                             <p className="my-stacks">
-                                Possuo conhecimento nas seguintes tecnologias:
+                                {isMobile ? 'Trabalhando com as seguintes stacks:' : 'Possuo conhecimento nas seguintes tecnologias:'}
                             </p>
                             <div className="carousel">
-                                <MyCarousel />  
+                                <MyCarousel />
                             </div>
                         </div>
                     </div>
