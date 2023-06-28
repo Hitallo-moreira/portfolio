a496833b5739ab2f7634367b13a683aff5fa08e4
Hitallo-moreira <hitallomoreira13@gmail.com>
2023-06-28 15:48:40 -0300

    feat: add sidebar to mobile resolution

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,30 +1,62 @@
-import CustomButton from './Button'
+import React, { useState } from 'react';
+import { slide as Menu } from 'react-burger-menu';
+import CustomButton from './Button';
 import file from '../assets/CvHitallo.Feitosa.pdf';
+import { useMediaQuery } from 'react-responsive';
 
 function Header() {
-    return (      
-        <header className="container">
-            <div className="content">
-                <div className="left-content">
-                    <div className="logo">
-                        <p><a href="#">H</a></p>
-                    </div>
-                    <nav>
-                        <ul>
-                            <li><a href="#about">Sobre</a></li>
-                            <li><a href="#projects">Projetos</a></li>
-                            <li><a href="#contact">Contato</a></li>
-                        </ul>
-                    </nav>
-                </div>
-                <div className="right-content">         
-                    <div className="btn-download">
-                        <CustomButton value="Baixar CV" isDownloadButton={true} file={file} />
-                    </div>
-                </div>
+  const isMobile = useMediaQuery({ maxWidth: 767 });
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleMenuClose = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <div className='container'>
+      {isMobile ? (
+        <header className='mobile-header'>
+          <div className="logo">
+            <p><a href="#">H</a></p>
+          </div>
+          <Menu left width={'250px'} className="mobile-menu" isOpen={isOpen} onStateChange={state => setIsOpen(state.isOpen)}>
+            <a className='download' href={file} download onClick={handleMenuClose}>Baixar CV</a>
+            <a className="menu-item" href="#about" onClick={handleMenuClose}>
+              Sobre
+            </a>
+            <a className="menu-item" href="#projects" onClick={handleMenuClose}>
+              Projetos
+            </a>
+            <a className="menu-item" href="#contact" onClick={handleMenuClose}>
+              Contato
+            </a>
+          </Menu>
+        </header>
+      ) : (
+        <header>
+          <div className="content">
+            <div className="left-content">
+              <div className="logo">
+                <p><a href="#">H</a></p>
+              </div>
+              <nav>
+                <ul>
+                  <li><a href="#about">Sobre</a></li>
+                  <li><a href="#projects">Projetos</a></li>
+                  <li><a href="#contact">Contato</a></li>
+                </ul>
+              </nav>
             </div>
-        </header>    
-    )
+            <div className="right-content">
+              <div className="btn-download">
+                <CustomButton value="Baixar CV" isDownloadButton={true} file={file} />
+              </div>
+            </div>
+          </div>
+        </header>
+      )}
+    </div>
+  );
 }
 
-export default Header
+export default Header;
